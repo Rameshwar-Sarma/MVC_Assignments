fa0887fe967bbbce6c85c1bd4b113cddf5014247
rameshwar <ramu@gmail.com>
2023-08-17 11:26:30 +0530

    Assignments Commiting

---
+++ StaticScopeAssignment1/Solution1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1
+{
+    public class Program
+    {
+        public static int var1 = 10;
+        public static String str1 = "C# .Net" ;
+        static void Main(string[] args)
+        {
+            var1 = 25;
+            str1 = "Welcome";
+            Console.WriteLine("var1 before calling scope method = " + var1);
+            Console.WriteLine("str1 before calling scope method = " + str1);
+            StaticScope scope = new StaticScope();
+            scope.method1();
+            Console.WriteLine("var1 after calling scope method = " + var1);
+            Console.WriteLine("str1 after calling scope method = " + str1);
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ StaticScopeAssignment1/Solution1/StaticScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1
+{
+
+    class StaticScope
+    {
+        public void method1() 
+        { 
+        Console.WriteLine("Value of var1 before Assignment :"+Program.var1);
+        Console.WriteLine("Value of str1 before Assignment :" + Program.str1);
+        Program.var1 = 26;
+        Program.str1 = "in Static Scope";
+        Console.WriteLine("Value of var1 after Assignment :" + Program.var1);
+        Console.WriteLine("Value of str1 after Assignment :" + Program.str1);
+        }
+    }
+}
