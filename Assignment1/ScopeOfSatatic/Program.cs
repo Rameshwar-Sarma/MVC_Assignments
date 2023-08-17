4a14d3bcc15a0414739c76dc329a7c02707f43e6
rameshwar <ramu@gmail.com>
2023-08-17 10:50:56 +0530

    Assignemnt1 Committed

---
+++ Assignment1/ScopeOfSatatic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Solution1;
+
+namespace ScopeOfSatatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var1 = " + Program.var1 + "\nstr1 = " + str1);
+        }
+    }
+}
