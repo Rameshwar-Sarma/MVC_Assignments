5d0eabf485775d847ccd71690f5f2ce85d8def22
Rameshwara Sarma <84918440+Rameshwar-Sarma@users.noreply.github.com>
2023-10-03 19:16:08 +0530

    Delete ThreeTierArchitectureProject directory

--- ThreeTierArchitectureProject/DataAccessLayer/Connection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.SqlClient;
-
-namespace DataAccessLayer
-{
-    public static class Connection
-    {
-        public static SqlConnection SQLConObj = new SqlConnection("Data Source=192.168.0.30;Initial Catalog=empid250;Persist Security Info=True;User ID=User5;Password=CDev005#8");
-    }
-}
